5f16aedd8376e0ea3177c622aa096e56887fc9fa
Teror Fox <i@trfox.top>
2025-03-07 09:12:30 +0800

    Create custom.js

---
+++ blog/custom.js
@@ -0,0 +1,13 @@
+// 检查当前页面是否是首页
+if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+  axios.get('https://v1.hitokoto.cn')
+    .then(({ data }) => {
+      // 获取目标元素
+      const hitokotoElement = document.querySelector('small.text-center');
+      if (hitokotoElement) {
+        // 替换元素内容为获取的句子
+        hitokotoElement.innerText = data.hitokoto;
+      }
+    })
+    .catch(console.error);
+}
